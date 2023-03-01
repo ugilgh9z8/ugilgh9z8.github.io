9183329627795788133aba4f7902cf308cf52edb
ugilgh9z8 <der.motivator@web.de>
2023-03-01 21:20:59 +0100

    jobSceduler

--- src/Table.js
+++ src/Table.js
@@ -11,38 +11,31 @@ const TableHeader = () => {
     )
   }
 
-  const TableBody = () => {
-    return (
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
+  const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
         </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    )
+      )
+    })
+  
+    return <tbody>{rows}</tbody>
   }
 
-class Table extends Component {
-  render() {
+  const Table = (props) => {
+    const { characterData, removeCharacter } = props
+  
     return (
-         <table>
+      <table>
         <TableHeader />
-        <TableBody />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
       </table>
     )
   }
-}
 
 export default Table
